398a7a1d0f49e888a87ddecc44178ee95bcd3dfd
Sahil Soni <bt25cse001@students.vnit.ac.in>
2026-06-10 02:58:52 +0530

    orderplace problem

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main:     resolve(__dirname, 'index.html'),
         services: resolve(__dirname, 'services.html'),
+        order:    resolve(__dirname, 'order.html'),
       }
     }
   }
